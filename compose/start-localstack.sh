61393904cb9927e7878b339a4ad0c92b0b4d7594
whitewater design <whitewaterdesign>
2025-09-05 13:45:36 +0100

    build: DF-379 - Setup base forms-adaptor-template

---
+++ compose/start-localstack.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export AWS_REGION=eu-west-2
+export AWS_DEFAULT_REGION=eu-west-2
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+
+# topics
+aws --endpoint-url=http://localhost:4566 sns create-topic --name forms_runner_submission_events
+
+# queues
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name forms_adaptor_events
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name forms_adaptor_events-deadletter
+
+# subscriptions
+aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn "arn:aws:sns:eu-west-2:000000000000:forms_runner_submission_events" \
+  --protocol sqs --attributes RawMessageDelivery=true --notification-endpoint "arn:aws:sqs:eu-west-2:000000000000:forms_adaptor_events"
